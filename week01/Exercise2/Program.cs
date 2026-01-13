723b1ef9b3d86cb9391f9f7b669b334866044732
chi-kwa-do002 <wudeozor@byupathway.edu>
2026-01-13 16:22:12 +0000

    built a grading program

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is you score? ");
+        int scoreInput = int.Parse(Console.ReadLine());
+        string letter = "";
+        if (scoreInput >= 93)
+        {
+            letter = "A";
+        }
+        else if (scoreInput >= 90)
+        {
+            letter = "A-";
+        }
+        else if (scoreInput >= 87)
+        {
+            letter = "B+";
+        }
+        else if (scoreInput >= 83)
+        {
+            letter = "B";
+        }
+        else if (scoreInput >= 80)
+        {
+            letter = "B-";
+        }
+        else if (scoreInput >= 77)
+        {
+            letter = "C+";
+        }
+        else if (scoreInput >= 73)
+        {
+            letter = "C";
+        }
+        else if (scoreInput >= 70)
+        {
+            letter = "C-";
+        }
+        else if (scoreInput >= 67)
+        {
+            letter = "D+";
+        }
+        else if (scoreInput >= 63)
+        {
+            letter = "D";
+        }
+        else if (scoreInput >= 60)
+        {
+            letter = "D-";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}.");
+        
+        if (letter == "A" || letter == "B" || letter == "C" || letter == "D")
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
