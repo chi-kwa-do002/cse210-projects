562596dcf514b1511b912e7736d397c9bc40e55f
chi-kwa-do002 <wudeozor@byupathway.edu>
2026-01-14 00:35:05 +0000

    I built a guess game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,27 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
-
+        string playAgain = "";
         int userGuess = -1;
-
-        while (userGuess != magicNumber)
+        do
         {
-            Console.Write("Guess the magic number (between 1 and 100): ");
-            userGuess = int.Parse(Console.ReadLine());
-
-            if (userGuess < magicNumber)
+            int randomNumber = randomGenerator.Next(1, 101); 
+            userGuess = -1;
+            
+            while (userGuess != randomNumber)
             {
-                Console.WriteLine("Too low! Try again.");
+                Console.Write("Guess a number between 1 and 100: ");
+                userGuess = int.Parse(Console.ReadLine());
+                if (userGuess < randomNumber)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if (userGuess > randomNumber)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You've guessed the number!");
+                }
             }
-            else if (userGuess > magicNumber)
-            {
-                Console.WriteLine("Too high! Try again.");
-            }
-            else
-            {
-                Console.WriteLine("Congratulations! You've guessed the magic number!");
-            }
+            Console.Write("Do you want to play again? (yes/no)");
+            playAgain = Console.ReadLine().ToLower();
+        }while (playAgain == "yes");
+        
+        if (playAgain != "yes")
+        {
+            Console.WriteLine("Goodbye! Thank you for playing");
         }
+
     }
 }
